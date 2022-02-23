f0aea4719604d780cb9c9caf53c1dacb602bc32f
Langchao Wu <langchaowu@yahoo.com>
2022-02-22 19:45:27 -0500

    commit tree 85f21032041c73c8dfc40d595374a89a6960f611

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if(num>2500){
+    return 'No can do.';
+  }else if(num >2000){
+    return 'I will gladly take your thirty bucks.';
+  }else if(num<=400){
+    return 'This one is on me!';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(str){
   // Write your code here!
+    return str=== 'NYC'? "Ok, sounds good.":"No go." ;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(str){
   // Write your code here!
+  switch(str){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: return 'Bye.';
+  }
 }
